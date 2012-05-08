6457d9baac1523444c83ff70de302f439a72a7dc
Randall Wood <randall.h.wood@alexandriasoftware.com>
2012-05-08 07:04:26 -0400

    Flesh out services for basic ZeroConf support.

--- JMRIService.h
+++ JMRIService.h
@@ -7,12 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SimpleService.h"
+#import "WiThrottleService.h"
+#import "XMLIOService.h"
 
-@interface JMRIService : NSObject
+extern NSString *const JMRIServiceSimple;
+extern NSString *const JMRIServiceWiThrottle;
+extern NSString *const JMRIServiceXmlIO;
+
+@interface JMRIService : NSObject {
+    
+    SimpleService *simple;
+    WiThrottleService *wiThrottle;
+    XMLIOService *xmlio;
+    
+}
 
 #pragma mark - Initialization
 
 - (id)initWithAddress:(NSString *)address withPorts:(NSDictionary *)ports;
 - (id)initWithWebServices:(NSDictionary *)services;
+
+#pragma mark - Properties
+
+@property (retain, readwrite) SimpleService *simpleService;
+@property (retain, readwrite) WiThrottleService *wiThrottleService;
+@property (retain, readwrite) XMLIOService *xmlIOService;
+@property (readonly) Boolean hasSimpleService;
+@property (readonly) Boolean hasWiThrottleService;
+@property (readonly) Boolean hasXmlIOService;
+@property (retain, readonly) NSArray *addresses;
+@property (retain, readonly) NSString *domain;
+@property (retain, readonly) NSString *hostName;
+@property (retain, readonly) NSString *name;
 
 @end
--- JMRIService.m
+++ JMRIService.m
@@ -8,6 +8,94 @@
 
 #import "JMRIService.h"
 
+NSString *const JMRIServiceSimple = @"JMRINetwork";
+NSString *const JMRIServiceXmlIO = @"WebServer";
+NSString *const JMRIServiceWiThrottle = @"wiThrottle";
+
 @implementation JMRIService
+
+@synthesize simpleService = simple;
+@synthesize wiThrottleService = wiThrottle;
+@synthesize xmlIOService = xmlio;
+
+- (id)initWithAddress:(NSString *)address withPorts:(NSDictionary *)ports {
+    if (([super init] != nil)) {
+        if ([ports valueForKey:JMRIServiceSimple]) {
+            self.simpleService = [[SimpleService alloc] initWithAddress:address withPort:[[ports valueForKey:JMRIServiceSimple] integerValue]];
+        }
+        if ([ports valueForKey:JMRIServiceWiThrottle]) {
+            self.wiThrottleService = [[WiThrottleService alloc] initWithAddress:address withPort:[[ports valueForKey:JMRIServiceWiThrottle] integerValue]];
+        }
+        if ([ports valueForKey:JMRIServiceXmlIO]) {
+            self.xmlIOService = [[XMLIOService alloc] initWithAddress:address withPort:[[ports valueForKey:JMRIServiceXmlIO] integerValue]];
+        }
+    }
+    return self;
+}
+
+- (id)initWithWebServices:(NSDictionary *)services {
+    if (([super init] != nil)) {
+        self.simpleService = [services valueForKey:JMRIServiceSimple];
+        self.wiThrottleService = [services valueForKey:JMRIServiceWiThrottle];
+        self.xmlIOService = [services valueForKey:JMRIServiceXmlIO];
+    }
+    return self;
+}
+
+- (Boolean)hasSimpleService {
+    return (self.simpleService != nil);
+}
+
+- (Boolean)hasWiThrottleService {
+    return (self.wiThrottleService != nil);
+}
+
+- (Boolean)hasXmlIOService {
+    return (self.xmlIOService != nil);
+}
+
+- (NSArray *)addresses {
+    if (self.hasSimpleService) {
+        return self.simpleService.addresses;
+    } else if (self.hasWiThrottleService) {
+        return self.wiThrottleService.addresses;
+    } else if (self.hasXmlIOService) {
+        return self.xmlIOService.addresses;
+    }
+    return nil;
+}
+
+- (NSString *)domain {
+    if (self.hasSimpleService) {
+        return self.simpleService.domain;
+    } else if (self.hasWiThrottleService) {
+        return self.wiThrottleService.domain;
+    } else if (self.hasXmlIOService) {
+        return self.xmlIOService.domain;
+    }
+    return nil;
+}
+
+- (NSString *)hostName {
+    if (self.hasSimpleService) {
+        return self.simpleService.hostName;
+    } else if (self.hasWiThrottleService) {
+        return self.wiThrottleService.hostName;
+    } else if (self.hasXmlIOService) {
+        return self.xmlIOService.hostName;
+    }
+    return nil;
+}
+
+- (NSString *)name {
+    if (self.hasSimpleService) {
+        return self.simpleService.name;
+    } else if (self.hasWiThrottleService) {
+        return self.wiThrottleService.name;
+    } else if (self.hasXmlIOService) {
+        return self.xmlIOService.name;
+    }
+    return nil;
+}
 
 @end
