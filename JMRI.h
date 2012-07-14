cbef1314241ce1e374386d2b436e44ee0ae72971
Randall Wood <randall.h.wood@alexandriasoftware.com>
2012-07-14 12:15:03 -0400

    Make certain headers public.

---
+++ JMRI.h
@@ -0,0 +1,19 @@
+//
+//  JMRI.h
+//  JMRI Framework
+//
+//  Created by Randall Wood on 14/7/2012.
+//  Copyright (c) 2012 Alexandria Software. All rights reserved.
+//
+
+#include <Foundation/Foundation.h>
+
+//  JMRI core
+
+#import <JMRI/JMRIService.h>
+#import <JMRI/JMRIServiceBrowser.h>
+
+//  JMRI SimpleService
+
+#import <JMRI/SimpleService.h>
+#import <JMRI/SimpleServiceBrowser.h>
