5845887466bc373cfe1b41910f9b4895211e87da
Randall Wood <randall.h.wood@alexandriasoftware.com>
2012-08-01 06:35:54 -0400

    Close #6, close #7, partly address #3.

--- JMRIService.h
+++ JMRIService.h
@@ -11,7 +11,29 @@
 #import "WiThrottleService.h"
 #import "XMLIOService.h"
 
-@interface JMRIService : NSObject {
+@class JMRIService;
+@class JMRIItem;
+@class JMRIPower;
+
+#pragma mark - Delegatation Protocol
+
+@protocol JMRIServiceDelegate
+
+@required
+- (void)JMRIService:(JMRIService *)service didNotResolve:(NSDictionary *)errorDict;
+- (void)JMRIService:(JMRIService *)service didFailWithError:(NSError *)error;
+
+@optional
+- (void)JMRIServiceDidResolveAddress:(JMRIService *)service;
+- (void)JMRIServiceWillResolve:(JMRIService *)service;
+- (void)JMRIServiceDidOpenConnection:(JMRIService *)service;
+- (void)JMRIService:(JMRIService *)service didGetInput:(NSString *)input;
+
+@end
+
+#pragma mark - Interface
+
+@interface JMRIService : NSObject <SimpleServiceDelegate, XMLIOServiceDelegate> {
     
     SimpleService *simple;
     XMLIOService *xmlio;
@@ -33,15 +55,42 @@
 
 #pragma mark - Properties
 
+@property (readonly) NSArray *addresses;
+@property (readonly) NSString *domain;
+@property (readonly) NSString *hostName;
+@property (readonly) NSString *name;
+@property (weak) id<NSObject, JMRIServiceDelegate> delegate;
+
+#pragma mark - Service properties
+
 @property (readwrite, strong) SimpleService *simpleService;
 @property (readwrite, strong) WiThrottleService *wiThrottleService;
 @property (readwrite, strong) XMLIOService *webService;
 @property (readonly) Boolean hasSimpleService;
 @property (readonly) Boolean hasWiThrottleService;
 @property (readonly) Boolean hasWebService;
-@property (readonly) NSArray *addresses;
-@property (readonly) NSString *domain;
-@property (readonly) NSString *hostName;
-@property (readonly) NSString *name;
+@property Boolean requiresSimpleService;
+@property Boolean requiresWiThrottleService;
+@property Boolean requiresXmlIOService;
+@property Boolean useSimpleService;
+@property Boolean useWiThrottleService;
+@property Boolean useXmlIOService;
+
+#pragma mark - JMRI element properties
+
+@property (readonly) NSDictionary *memoryVariables;
+@property (readonly) NSDictionary *panels;
+@property (readonly) JMRIPower *power;
+@property (readonly) NSDictionary *reporters;
+@property (readonly) NSDictionary *roster;
+@property (readonly) NSDictionary *routes;
+@property (readonly) NSDictionary *sensors;
+@property (readonly) NSDictionary *turnouts;
+
+#pragma mark - JMRI element handling
+
+- (void)monitor:(JMRIItem *)item;
+- (void)stopMonitoring:(JMRIItem *)item;
+- (Boolean)isMonitoring:(JMRIItem *)item;
 
 @end
--- JMRIService.m
+++ JMRIService.m
@@ -8,19 +8,38 @@
 
 #import "JMRIService.h"
 #import "JMRIConstants.h"
+#import "JMRIItem+Internal.h"
+#import "JMRIPower.h"
+#import "JMRITurnout.h"
+
+@interface JMRIService (Private)
+
+- (void)commonInit;
+
+@end
 
 @implementation JMRIService
 
 - (id)initWithAddress:(NSString *)address withPorts:(NSDictionary *)ports {
     if (([super init] != nil)) {
+        [self commonInit];
         if ([ports valueForKey:JMRIServiceSimple]) {
             self.simpleService = [[SimpleService alloc] initWithAddress:address withPort:[[ports valueForKey:JMRIServiceSimple] integerValue]];
+            self.requiresSimpleService = YES;
+        } else {
+            self.useSimpleService = NO;
         }
         if ([ports valueForKey:JMRIServiceWiThrottle]) {
             self.wiThrottleService = [[WiThrottleService alloc] initWithAddress:address withPort:[[ports valueForKey:JMRIServiceWiThrottle] integerValue]];
+            self.requiresWiThrottleService = YES;
+        } else {
+            self.useWiThrottleService = NO;
         }
         if ([ports valueForKey:JMRIServiceWeb]) {
             self.webService = [[XMLIOService alloc] initWithAddress:address withPort:[[ports valueForKey:JMRIServiceWeb] integerValue]];
+            self.requiresXmlIOService = YES;
+        } else {
+            self.useXmlIOService = NO;
         }
     }
     return self;
@@ -28,14 +47,40 @@
 
 - (id)initWithWebServices:(NSDictionary *)services {
     if (([super init] != nil)) {
-        self.simpleService = [services valueForKey:JMRIServiceSimple];
-        [self.simpleService startMonitoring];
-        self.wiThrottleService = [services valueForKey:JMRIServiceWiThrottle];
-        [self.wiThrottleService startMonitoring];
-        self.webService = [services valueForKey:JMRIServiceWeb];
-        [self.webService startMonitoring];
+        [self commonInit];
+        if ([services valueForKey:JMRIServiceSimple]) {
+            self.simpleService = [services valueForKey:JMRIServiceSimple];
+            [self.simpleService startMonitoring];
+            self.requiresSimpleService = YES;
+        } else {
+            self.useSimpleService = NO;
+        }
+        if ([services valueForKey:JMRIServiceWiThrottle]) {
+            self.wiThrottleService = [services valueForKey:JMRIServiceWiThrottle];
+            [self.wiThrottleService startMonitoring];
+            self.requiresWiThrottleService = YES;
+        } else {
+            self.useWiThrottleService = NO;
+        }
+        if ([services valueForKey:JMRIServiceWeb]) {
+            self.webService = [services valueForKey:JMRIServiceWeb];
+            [self.webService startMonitoring];
+            self.requiresXmlIOService = YES;
+        } else {
+            self.useXmlIOService = NO;
+        }
     }
     return self;
+}
+
+- (void)commonInit {
+    power = [[JMRIPower alloc] initWithName:JMRITypePower withService:self];
+    self.requiresSimpleService = NO;
+    self.requiresWiThrottleService = NO;
+    self.requiresXmlIOService = NO;
+    self.useSimpleService = YES;
+    self.useWiThrottleService = YES;
+    self.useXmlIOService = YES;
 }
 
 #pragma mark - Object Handling
@@ -44,19 +89,7 @@
 	return [self.name localizedCaseInsensitiveCompare:aService.name];
 }
 
-#pragma - Properties
-
-- (Boolean)hasSimpleService {
-    return (self.simpleService != nil);
-}
-
-- (Boolean)hasWiThrottleService {
-    return (self.wiThrottleService != nil);
-}
-
-- (Boolean)hasWebService {
-    return (self.webService != nil);
-}
+#pragma mark - Properties
 
 - (NSArray *)addresses {
     if (self.hasSimpleService) {
@@ -81,6 +114,10 @@
     return name;
 }
 
+@synthesize delegate;
+
+#pragma mark - Service properties
+
 - (SimpleService *)simpleService {
     return simple;
 }
@@ -91,7 +128,9 @@
     }
     [simple stopMonitoring];
     [simple stop];
+    simple.delegate = nil;
     simple = simpleService;
+    simple.delegate = self;
     [simple startMonitoring];
     if (simple) {
         domain = simple.domain;
@@ -110,7 +149,9 @@
     }
     [xmlio stopMonitoring];
     [xmlio stop];
+    xmlio.delegate = nil;
     xmlio = webService;
+    xmlio.delegate = self;
     [xmlio startMonitoring];
     if (xmlio) {
         domain = xmlio.domain;
@@ -129,7 +170,9 @@
     }
     [wiThrottle stopMonitoring];
     [wiThrottle stop];
+    wiThrottle.delegate = nil;
     wiThrottle = wiThrottleService;
+    wiThrottle.delegate = self;
     [wiThrottle startMonitoring];
     if (wiThrottle) {
         domain = wiThrottle.domain;
@@ -137,5 +180,138 @@
         name = wiThrottle.name;
     }
 }
+
+
+- (Boolean)hasSimpleService {
+    return (self.simpleService != nil);
+}
+
+- (Boolean)hasWiThrottleService {
+    return (self.wiThrottleService != nil);
+}
+
+- (Boolean)hasWebService {
+    return (self.webService != nil);
+}
+
+@synthesize requiresSimpleService = _requiresSimpleService;
+@synthesize requiresWiThrottleService = _requiresWiThrottleService;
+@synthesize requiresXmlIOService = _requiresXmlIOService;
+@synthesize useSimpleService = _useSimpleService;
+@synthesize useWiThrottleService = _useWiThrottleService;
+@synthesize useXmlIOService = _useXmlIOService;
+
+#pragma mark - JMRI Elements
+
+@synthesize memoryVariables;
+@synthesize panels;
+@synthesize power;
+@synthesize reporters;
+@synthesize roster;
+@synthesize routes;
+@synthesize sensors;
+@synthesize turnouts;
+
+- (void)monitor:(JMRIItem *)item {
+    if (self.hasWebService && self.useXmlIOService) {
+        [self.webService startMonitoring:item.name ofType:item.type];
+    }
+}
+
+- (void)stopMonitoring:(JMRIItem *)item {
+    if (self.hasWebService) {
+        [self.webService stopMonitoring:item.name ofType:item.type];
+    }
+}
+
+- (Boolean)isMonitoring:(JMRIItem *)item {
+    if (self.hasWebService) {
+        return [self.webService isMonitoring:item.name ofType:item.type];
+    }
+    return NO;
+}
+
+#pragma mark - JMRINetService delegate
+
+- (void)JMRINetService:(JMRINetService *)service didNotResolve:(NSDictionary *)errorDict {
+    // JMRIService probably needs to handle this itself, but I'm not sure how
+    if ([self.delegate respondsToSelector:@selector(JMRIService:didNotResolve:)]) {
+        [self.delegate JMRIService:self didNotResolve:errorDict];
+    }
+}
+
+- (void)JMRINetService:(JMRINetService *)service didGetPowerState:(NSUInteger)state {
+    [self.power setState:state updateService:NO];
+}
+
+- (void)JMRINetService:(JMRINetService *)service didGetTurnout:(NSString *)turnout withState:(NSUInteger)state {
+    if (![self.turnouts objectForKey:turnout]) {
+        JMRITurnout *turnoutObj = [[JMRITurnout alloc] initWithName:turnout withService:self];
+        [self.turnouts setValue:turnoutObj forKey:turnout];
+    }
+    [((JMRITurnout *)[self.turnouts objectForKey:turnout]) setState:state updateService:NO];
+}
+
+#pragma mark - Simple service delegate
+
+- (void)simpleService:(SimpleService *)service didFailWithError:(NSError *)error {
+    if ([self.delegate respondsToSelector:@selector(JMRIService:didFailWithError:)]) {
+        [self.delegate JMRIService:self didFailWithError:error];
+    }
+}
+
+- (void)simpleService:(SimpleService *)service didGetInput:(NSString *)input {
+    if ([self.delegate respondsToSelector:@selector(JMRIService:didGetInput:)]) {
+        [self.delegate JMRIService:self didGetInput:input];
+    }
+}
+
+- (void)simpleServiceDidOpenConnection:(SimpleService *)service {
+    if ([self.delegate respondsToSelector:@selector(JMRIServiceDidOpenConnection:)]) {
+        [self.delegate JMRIServiceDidOpenConnection:self];
+    }
+}
+
+#pragma mark - Web service delegate
+
+- (void)XMLIOService:(XMLIOService *)service didFailWithError:(NSError *)error {
+    if ([self.delegate respondsToSelector:@selector(JMRIService:didFailWithError:)]) {
+        [self.delegate JMRIService:self didFailWithError:error];
+    }
+}
+
+- (void)XMLIOService:(XMLIOService *)service didListItems:(NSArray *)items ofType:(NSString *)type {
+    if ([type isEqualToString:JMRITypePower]) {
+        [self.power setState:[((XMLIOItem *)[items objectAtIndex:0]).value integerValue] updateService:NO];
+    } else if ([type isEqualToString:JMRITypeTurnout]) {
+        for (XMLIOItem *i in items) {
+            if ([self.turnouts objectForKey:i.name]) {
+                ((JMRITurnout *)[self.turnouts objectForKey:i.name]).state = [i.value integerValue];
+            } else {
+                [self.turnouts setValue:[i JMRIItemForService:self] forKey:i.name];
+            }
+        }
+    }
+}
+
+- (void)XMLIOService:(XMLIOService *)service didReadItem:(XMLIOItem *)item withName:(NSString *)aName ofType:(NSString *)type withValue:(NSString *)value {
+    if ([type isEqualToString:JMRITypePower]) {
+        [self.power setState:[item.value integerValue] updateService:NO];
+    } else if ([type isEqualToString:JMRITypeTurnout]) {
+        if ([self.turnouts objectForKey:aName]) {
+            ((JMRITurnout *)[self.turnouts objectForKey:aName]).state = [item.value integerValue];
+        } else {
+            [self.turnouts setValue:[item JMRIItemForService:self] forKey:aName];
+        }
+    }
+}
+
+- (void)XMLIOService:(XMLIOService *)service didWriteItem:(XMLIOItem *)item withName:(NSString *)aName ofType:(NSString *)type withValue:(NSString *)value {
+    [self XMLIOService:service didReadItem:item withName:aName ofType:type withValue:value];
+}
+
+- (void)XMLIOService:(XMLIOService *)service didGetThrottle:(XMLIOThrottle *)throttle withAddress:(NSUInteger)address {}
+- (void)XMLIOService:(XMLIOService *)service didConnectWithRequest:(NSURLRequest *)request {}
+- (void)XMLIOServiceDidFinishLoading:(XMLIOService *)service {}
 
 @end
