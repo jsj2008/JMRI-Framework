19bc6c52585bbf6622549ba4d15763ca40ccce08
Randall Wood <randall.h.wood@alexandriasoftware.com>
2013-04-30 06:55:04 -0400

    Inline documentation for JMRIServiceBrowser
    Did not document the delegation protocol, since that is being switched
    to a notification-based scheme.

--- Classes/JMRIServiceBrowser.h
+++ Classes/JMRIServiceBrowser.h
@@ -14,6 +14,9 @@
 @class WiThrottleServiceBrowser;
 @class WebServiceBrowser;
 
+/**
+ Find published JMRI servers on a network using zeroconf networking.
+ */
 @interface JMRIServiceBrowser : NSObject {
     
 	BOOL searching;
@@ -24,30 +27,111 @@
     
 }
 
-#pragma mark - Initializers
+#pragma mark - Initialization & disposal
+/** @name Initialization & disposal */
 
+/**
+ Create a browser, allowing it to enumerate all JMRI servers, irregardless of the services enabled on the server.
+ */
+- (id)init;
+
+/**
+ Create a browser, requiring that it only enumerate JMRI servers providing specified services.
+ 
+ Valid services are JMRIServiceJson, JMRIServiceSimple, JMRIServiceWeb, JMRIServiceWiThrottle, and JMRIServiceXmlIO.
+ 
+ @param services A set of service types.
+ */
 - (id)initForServices:(NSSet *)services;
 
-#pragma mark - Service browser methods
+#pragma mark - Services & browsing
+/** @name Services & browsing */
 
+/**
+ Start searching for services.
+ */
 - (void)searchForServices;
+/**
+ Manually add an unnamed service.
+ 
+ This method calls addServiceWithName:withAddress:withPorts: using the address as the name.
+ 
+ @param address A hostname or IP address for the JMRI server.
+ @param ports A dictionary containing key/value pairs where a service is the key and the value is an NSNumber for the port.
+ @sa addServiceWithName:withAddress:withPorts:
+ */
 - (void)addServiceWithAddress:(NSString *)address withPorts:(NSDictionary *)ports;
+/**
+ Manually add a service.
+ 
+ This method allows JMRI servers that are not discoverable using zeroconf networking to be added to the list of services
+ available to the browser, allowing implementing applications to rely solely on the browser for service listings.
+ 
+ @param name The name of the JMRI server. This name can be a model railroad name instead of a hostname, for example.
+ @param address A hostname or IP address for the JMRI server.
+ @param ports A dictionary containing key/value pairs where a service is the key and the value is an NSNumber for the port.
+ */
 - (void)addServiceWithName:(NSString *)name withAddress:(NSString *)address withPorts:(NSDictionary *)ports;
+/**
+ Remove the service with the given name from the browser's list of services.
+ 
+ @param name The name of the JMRI server to remove.
+ */
 - (void)removeServiceWithName:(NSString *)name;
+/**
+ Stop ongoing searches for services.
+ 
+ Use this method to ensure that (for example) an iOS app will not continue to attempt service discovery when a view
+ listing JMRI servers is dismissed, or when the application looses focus.
+ */
 - (void)stop;
 
-#pragma mark - Utility methods
+#pragma mark - Utilities
+/** @name Utilities */
 
+/**
+ Test if the services array contains a service.
+ 
+ This test relys on the name of the service to make this determination.
+ 
+ @param service The service to test against.
+ */
 - (BOOL)containsService:(JMRIService *)service;
+/**
+ Sorts the services array by name.
+ */
 - (void)sortServices;
+/**
+ Get the index of the named service in the services array.
+ 
+ @param name The service name.
+ */
 - (NSUInteger)indexOfServiceWithName:(NSString *)name;
+/**
+ Get the service with the specified name.
+ 
+ @param name The service name.
+ */
 - (JMRIService *)serviceWithName:(NSString *)name;
 
 #pragma mark - Properties
+/** @name Properties */
 
+/**
+ The delegate.
+ */
 @property (weak, nonatomic) id delegate;
+/**
+ Check if a search for new services is ongoing or not. YES if searches are ongoing.
+ */
 @property (readonly) BOOL searching;
+/**
+ Known JMRI servers, both discovered, and manually entered.
+ */
 @property (nonatomic) NSMutableArray *services;
+/**
+ Services that must be provided by a JMRI server for the browser to retain it while searching.
+ */
 @property (readonly) NSSet *requiredServices;
 
 @end
