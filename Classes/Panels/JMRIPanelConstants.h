bd3b614b1ae5d3b619bba6b627385855c3f9477a
Randall Wood <randall.h.wood@alexandriasoftware.com>
2013-06-18 05:49:58 -0400

    Remove Panel support
    Retain a list of the named Panel constants previously defined.
    Addresses #25.

---
+++ Classes/Panels/JMRIPanelConstants.h
@@ -0,0 +1,15 @@
+//
+//  JMRIPanelHelper.h
+//  JMRI Framework
+//
+//  Created by Randall Wood on 4/8/2012.
+//
+//
+
+// elements
+extern NSString *const JMRIPanelIcon;
+extern NSString *const JMRIPanelIconMaps;
+extern NSString *const JMRIPanelIcons;
+
+// attributes
+extern NSString *const JMRIPanelRotation;
---
+++ Classes/Panels/JMRIPanelConstants.m
@@ -0,0 +1,15 @@
+//
+//  JMRIPanelHelper.m
+//  JMRI Framework
+//
+//  Created by Randall Wood on 4/8/2012.
+//
+//
+
+// elements
+NSString *const JMRIPanelIcon = @"icon";
+NSString *const JMRIPanelIconMaps = @"iconmaps";
+NSString *const JMRIPanelIcons = @"icons";
+
+// attributes
+NSString *const JMRIPanelRotation = @"rotation";
